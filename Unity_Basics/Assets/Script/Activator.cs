c107e336cac507ee090d9601f217f245d631d1d5
Nick T <u1401912@utah.edu>
2026-01-30 17:03:11 -0700

    Hello World in Unity

---
+++ Unity_Basics/Assets/Script/Activator.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    public Greeter HelloActor;
+
+    void Start()
+    {
+        Debug.Log(HelloActor.SayHello());
+    }
+}
---
+++ Unity_Basics/Assets/Script/Greeter.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Greeter : MonoBehaviour
+{
+    public string SayHello()
+    {
+        return new string("Hello World");
+    }
+}
