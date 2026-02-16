1fd2076caadd625ad689a5bef03655b56247c97e
Trippn56 <u1401912@utah.edu>
2026-02-15 18:36:50 -0700

    Unreal and part of Unity Done

---
+++ Unity_Basics/Assets/Script/Caller.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Caller : MonoBehaviour
+
+{
+    public Receiver receiver;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        receiver.OnCalled()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
